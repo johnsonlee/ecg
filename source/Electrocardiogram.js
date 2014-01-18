63f73fe48c7183cfbb5fb326deff488be8d6bad8
Johnson Lee <g.johnsonlee@gmail.com>
2014-01-18 16:58:41 +0800

    Commit ECG source code for demo

--- source/Electrocardiogram.js
+++ source/Electrocardiogram.js
@@ -3,61 +3,121 @@ define(function(require, exports, module) {
     /**
      * 
      */
-    function Electrocardiogram(options) {
-        var opt = options || {};
+    function Electrocardiogram(width, height) {
         var dom = document || window.document;
 
-        this.$width = opt.size.width;
-        this.$height = opt.size.height;
-        this.$padding = opt.padding || 5;
-        this.$gridColor = opt.grid.color || 'pink';
+        this.$width = width;
+        this.$height = height;
+        this.$gridColor = 'green';
+        this.$strokeColor = 'green';
         this.$graphics = dom.createElement('CANVAS').getContext('2d');
         this.$graphics.canvas.width = this.$width;
         this.$graphics.canvas.height = this.$height;
         this.$graphics.canvas.style.backgroundColor = 'transparent';
 
-        // move the canvas by 0.5px to fix blurring
-        this.$graphics.translate(0.5, 0.5);
-
         document.body.appendChild(this.$graphics.canvas);
+
+        this.drawGrid();
     };
 
     /**
      * The cell size is 1mm (=3.543307px)
      */
-    Electrocardiogram.CELL_SIZE = 3.543307;
+    Electrocardiogram.CELL_SIZE = 6;
 
     Electrocardiogram.BLOCK_SIZE = Electrocardiogram.CELL_SIZE * 5;
 
     (function() {
 
-        this.draw = function() {
+        /**
+         * Returns the width of this diagram
+         * 
+         * @return the width of this diagram
+         */
+        this.getWidth = function() {
+            return this.$graphics.canvas.width;
         };
 
+        /**
+         * Returns the height of this diagram
+         * 
+         * @return the height of this diagram
+         */
+        this.getHeight = function() {
+            return this.$graphics.canvas.height;
+        };
+
+        this.clear = function() {
+            this.$graphics.clearRect(0, 0, this.getWidth(), this.getHeight());
+        };
+
+        /**
+         * Draw curve with specified data
+         */
+        this.drawCurve = function(data) {
+            this.clear();
+            this.drawGrid();
+
+            var p0 = data[0];
+            var lw = this.$graphics.lineWidth;
+            var ss = this.$graphics.strokeStyle;
+
+            this.$graphics.strokeStyle = this.$strokeColor;
+
+            for (var i = 1; i < data.length; i++) {
+                var p1 = data[i];
+
+                this.$graphics.beginPath();
+                this.$graphics.lineWidth = 0.5;
+                this.drawLine((i - 1) * Electrocardiogram.CELL_SIZE, p0.pqrst, i * Electrocardiogram.CELL_SIZE, p1.pqrst);
+                this.$graphics.closePath();
+                this.$graphics.stroke();
+                p0 = p1;
+            }
+
+            this.$graphics.lineWidth = lw;
+            this.$graphics.strokeStyle = ss;
+        };
+
+        /**
+         * Draw the grid
+         */
+        this.drawGrid = function() {
+            var x = 0;
+            var y = 0;
+            var w = this.getWidth();
+            var h = this.getHeight();
+            var lw = this.$graphics.lineWidth;
+            var ss = this.$graphics.strokeStyle;
+
+            this.$graphics.strokeStyle = this.$gridColor;
+
+            for (var y = 0; y < h; y += Electrocardiogram.CELL_SIZE) {
+                this.$graphics.beginPath();
+                this.$graphics.lineWidth = (y % Electrocardiogram.BLOCK_SIZE) ? 0.1 : 0.2;
+                this.drawLine(0, y, w, y);
+                this.$graphics.closePath();
+                this.$graphics.stroke();
+            }
+
+            for (var x = 0; x < w; x += Electrocardiogram.CELL_SIZE) {
+                this.$graphics.beginPath();
+                this.$graphics.lineWidth = (x % Electrocardiogram.BLOCK_SIZE) ? 0.1 : 0.2;
+                this.drawLine(x, 0, x, h);
+                this.$graphics.closePath();
+                this.$graphics.stroke();
+            }
+
+            this.$graphics.lineWidth = lw;
+            this.$graphics.strokeStyle = ss;
+        };
+
+        /**
+         * Draw a line from point (x1, y1) to point (x2, y2)
+         */
         this.drawLine = function(x1, y1, x2, y2) {
             this.$graphics.moveTo(x1, y1);
             this.$graphics.lineTo(x2, y2);
-        };
-
-        this.drawGrid = function() {
-            var n = 0;
-            var x0 = this.$padding;
-            var y0 = this.$padding;
-            var x1 = this.$width - this.$padding;
-            var y1 = this.$height - this.$padding;
-            var ls = this.$graphics.strokeStyle;
-
-            this.$graphics.strokeStyle = this.$gridColor;
-            this.$graphics.beginPath();
-
-            for (var y = y0; y < y1; y += Electrocardiogram.CELL_SIZE) {
-                this.$graphics.lineWidth = (n++ % 5) ? 0.5 : 1;
-                this.drawLine(x0, y, x1, y);
-            }
-
-            this.$graphics.closePath();
-            this.$graphics.stroke();
-            this.$graphics.strokeStyle = ls;
         };
 
     }).call(Electrocardiogram.prototype);
